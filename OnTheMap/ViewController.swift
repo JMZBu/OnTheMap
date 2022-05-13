459084346fd5ed42393e0a8adc44822f9803fa7d
James Butler <jamesbutler1111@gmail.com>
2022-05-13 09:30:09 -0700

    created test login segue

--- OnTheMap/ViewController.swift
+++ OnTheMap/ViewController.swift
@@ -8,10 +8,16 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var loginButton: UIButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func loginComplete() {
+        performSegue(withIdentifier: "loginComplete", sender: nil)
     }
 
 

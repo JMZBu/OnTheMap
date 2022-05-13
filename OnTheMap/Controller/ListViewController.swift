c9c817a9e007027e3f7726da4c620843254706fb
James Butler <jamesbutler1111@gmail.com>
2022-05-13 10:09:21 -0700

    Created ViewController.swift files for each View Controller and linked them up

---
+++ OnTheMap/Controller/ListViewController.swift
@@ -0,0 +1,14 @@
+//
+//  ListViewController.swift
+//  OnTheMap
+//
+//  Created by James Butler on 5/13/22.
+//
+
+import Foundation
+import UIKit
+
+class ListViewController: UITableViewController {
+    
+    
+}
---
+++ OnTheMap/Controller/MapViewController.swift
@@ -0,0 +1,14 @@
+//
+//  MapViewController.swift
+//  OnTheMap
+//
+//  Created by James Butler on 5/13/22.
+//
+
+import Foundation
+import UIKit
+import MapKit
+
+class MapViewController: UIViewController, MKMapViewDelegate {
+    
+}

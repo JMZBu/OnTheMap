64f81c35b139c91588d230449602ca52228d5672
James Butler <jamesbutler1111@gmail.com>
2022-05-14 09:34:51 -0700

    Changed ListViewController to a View Controller instead of Table View Controller in storyboard (and wired it up)

--- OnTheMap/Controller/ListViewController.swift
+++ OnTheMap/Controller/ListViewController.swift
@@ -8,7 +8,47 @@
 import Foundation
 import UIKit
 
-class ListViewController: UITableViewController {
+class ListViewController: UIViewController {
     
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        OTMClient.getUserDetails { data, error in
+            DispatchQueue.main.async {
+                UsersListModel.usersList = data
+                self.tableView.reloadData()
+            }
+        }
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        tableView.reloadData()
+    }
+}
+
+extension ListViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return UsersListModel.usersList.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "listTableViewCell")
+        
+        let user = UsersListModel.usersList[indexPath.row]
+        
+        cell?.textLabel?.text = "\(user.firstName) \(user.lastName)"
+        cell?.detailTextLabel?.text = "\(user.mediaURL)"
+        
+        return cell!
+    }
     
 }

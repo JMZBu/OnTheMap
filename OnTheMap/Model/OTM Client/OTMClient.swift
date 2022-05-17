16232cff33a222d9217eecfd857a113e788ad0a2
James Butler <jamesbutler1111@gmail.com>
2022-05-17 13:24:51 -0700

    OTMClient: Created generic POST request func

--- OnTheMap/Model/OTM Client/OTMClient.swift
+++ OnTheMap/Model/OTM Client/OTMClient.swift
@@ -87,4 +87,34 @@ class OTMClient {
     }
     
 //MARK: POST Requests
+    
+    class func taskForPOSTrequest<RequestType: Encodable, ResponseType: Decodable>(url: URL, response: ResponseType.Type, body: RequestType, completion: @escaping (ResponseType?, Error?) -> Void) {
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        let body = body
+        request.httpBody = try! JSONEncoder().encode(body)
+        
+        let task = URLSession.shared.dataTask(with: request) { data, response, error in
+            guard let data = data else {
+                DispatchQueue.main.async {
+                    completion(nil, error)
+                }
+                return
+            }
+            let decoder = JSONDecoder()
+            do {
+                let responseObject = try decoder.decode(ResponseType.self, from: data)
+                DispatchQueue.main.async {
+                    completion(responseObject, nil)
+                }
+            } catch {
+                DispatchQueue.main.async {
+                    completion(nil, error)
+                }
+            }
+
+        }
+        task.resume()
+    }
 }

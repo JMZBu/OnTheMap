a77d396b261b89e26bd28430dbf0575c451d43bc
James Butler <jamesbutler1111@gmail.com>
2022-05-17 13:41:30 -0700

    Created AddLocationViewController

---
+++ OnTheMap/Controller/AddLocationViewController.swift
@@ -0,0 +1,15 @@
+//
+//  AddLocationViewController.swift
+//  OnTheMap
+//
+//  Created by James Butler on 5/17/22.
+//
+
+import Foundation
+import MapKit
+
+class AddLocationViewController: UIViewController, MKMapViewDelegate {
+    
+    
+    
+}

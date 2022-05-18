1120e7f0bdf08b015b3845c52812426316200bbc
James Butler <jamesbutler1111@gmail.com>
2022-05-18 08:57:48 -0700

    Wired up more of the LoginView

--- OnTheMap/Controller/LoginViewController.swift
+++ OnTheMap/Controller/LoginViewController.swift
@@ -9,15 +9,31 @@ import UIKit
 
 class LoginViewController: UIViewController {
     
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
     @IBOutlet weak var loginButton: UIButton!
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        emailTextField.text = nil
+        passwordTextField.text = nil
     }
     
     @IBAction func loginComplete() {
         performSegue(withIdentifier: "loginComplete", sender: nil)
+    }
+    
+    func setLoggingIn(_ loggingIn: Bool) {
+        if loggingIn {
+            activityIndicator.startAnimating()
+        } else {
+            activityIndicator.stopAnimating()
+        }
+        emailTextField.isEnabled = !loggingIn
+        passwordTextField.isEnabled = !loggingIn
+        loginButton.isEnabled = !loggingIn
     }
 
 
